65356eeb93d1277523266302b7987f87cb329905
Carlo Elvisi <carloelvisi01@outlook.it>
2025-01-09 15:28:51 +0100

    milestone 4

--- controllers/postsControllers.js
+++ controllers/postsControllers.js
@@ -1,20 +1,28 @@
 // Importiamo il file di connessione al database
-const connection = require('../data/connection.js');
+const connection = require("../data/connection.js");
 
 // Index
 function index(req, res) {
-    // prepariamo la query
-    const sql = 'SELECT * FROM posts';
-    // eseguiamo la query!
-    connection.query(sql, (err, results) => {
-        if (err) return res.status(500).json({ error: 'Database query failed' });
-        res.json(results);
-    });
+  // prepariamo la query
+  const sql = "SELECT * FROM `posts'";
+  // eseguiamo la query!
+  connection.query(sql, (err, results) => {
+    if (err) return res.status(500).json({ error: "Database query failed" });
+    res.json(results);
+  });
 }
 
-
-
 // Show - Recupera i dettagli di un post specifico per ID
+function show(req, res) {
+  // recuperiamo l'id dall' URL
+  const id = req.params.id
+  const sql = 'SELECT * FROM posts WHERE id = ?';
+  connection.query(sql, [id], (err, results) => {
+  if (err) return res.status(500).json({ error: 'Database query failed' });
+  if (results.length === 0) return res.status(404).json({ error: 'Post not found' });
+  res.json(results[0]);
+  });
+  }
 function show(req, res) {
   const id = parseInt(req.params.id);
   const post = postsData.find((post) => post.id === id);
@@ -84,12 +92,11 @@ function destroy(req, res) {
   // recuperiamo l'id dall' URL
   const { id } = req.params;
   //Eliminiamo la pizza dal menu
-  connection.query('DELETE FROM posts WHERE id = ?', [id], (err) => {
-  if (err) return res.status(500).json({ error: 'Failed to delete pizza' });
-  res.sendStatus(204)
+  connection.query("DELETE FROM posts WHERE id = ?", [id], (err) => {
+    if (err) return res.status(500).json({ error: "Failed to delete pizza" });
+    res.sendStatus(204);
   });
-  }
-
+}
 
 function destroy(req, res) {
   const id = parseInt(req.params.id);
